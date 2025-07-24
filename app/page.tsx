4f99554a57fbd1dab7ac2801b265424471a41b17
HaidarAhmad123 <haidarahmad421@gmail.com>
2025-07-23 23:11:03 -0700

    update resume

--- app/page.tsx
+++ app/page.tsx
@@ -64,7 +64,7 @@ export default function Homepage() {
               Haidar Ahmad / Full-stack Developer
             </p>
             <a
-              href="/resume.pdf"
+              href="https://drive.google.com/file/d/1j7DgZkhthjE1POpyP-vjzdbZl6u056zH/view?usp=sharing"
               className={styles.resumeButton}
               download
               target="_blank"
